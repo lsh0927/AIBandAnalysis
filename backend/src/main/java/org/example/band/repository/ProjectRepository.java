667d304e81aac489ef41647e2d7f106c98fd889b
lsheon0927@naver.com <lsheon0927@naver.com>
2025-01-30 20:29:41 +0900

    feat: 레포지토리 메서드 추가

--- backend/src/main/java/org/example/band/repository/ProjectRepository.java
+++ backend/src/main/java/org/example/band/repository/ProjectRepository.java
@@ -1,7 +1,10 @@
 package org.example.band.repository;
 
+import java.util.List;
+
 import org.example.band.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProjectRepository extends JpaRepository<Project,Long> {
+	List<Project> findAllByUserId(Long userId);
 }
